4736b3705cff2dcdb0cacc6df6fd84821630362e
Tarek <tarek_tarho@hotmail.com>
2024-10-19 16:16:27 +0200

    update elctric pokemon food

--- src/pokemon/types/ElectricPokemon.java
+++ src/pokemon/types/ElectricPokemon.java
@@ -17,7 +17,7 @@ public class ElectricPokemon extends Pokemon {
     public ElectricPokemon(String name, int level, int hp, String food, String sound) {
         super(name, level, hp, food, sound);
         setType("Electric");
-        setFood("Pikachu Candy");
+        setFood("Candy");
         setSound("Zap");
         addAttack("VoltTackle");
         addAttack("ElectroBall");
